70ba519834ae6261037e52ac0e220d9d16bf6e9d
Ameer Taweel <ameertaweel2002@gmail.com>
2022-04-04 03:01:21 +0300

    refactor: use tabs in codeforces python submissions

--- codeforces/313b.py
+++ codeforces/313b.py
@@ -8,8 +8,8 @@ import os
 import sys
 from io import BytesIO, IOBase
 if sys.version_info[0] < 3:
-    from __builtin__ import xrange as range
-    from future_builtins import ascii, filter, hex, map, oct, zip
+	from __builtin__ import xrange as range
+	from future_builtins import ascii, filter, hex, map, oct, zip
 
 import math
 from bisect import bisect_left, bisect_right
@@ -20,31 +20,31 @@ from collections import Counter
 ################################################################################
  
 def main():
-    s = input()
-    sum = 0
-    for i in range(len(s) - 1):
-        if s[i] == s[i + 1]:
-            sum += 1
+	s = input()
+	sum = 0
+	for i in range(len(s) - 1):
+		if s[i] == s[i + 1]:
+			sum += 1
 
-    start_sum = sum
-    start = [start_sum]
-    for i in range(1, len(s) - 1):
-        if s[i - 1] == s[i]:
-            start_sum -= 1
-        start.append(start_sum)
+	start_sum = sum
+	start = [start_sum]
+	for i in range(1, len(s) - 1):
+		if s[i - 1] == s[i]:
+			start_sum -= 1
+		start.append(start_sum)
 
-    end_sum = sum
-    end = [end_sum]
-    for i in range(len(s) - 2, 0, -1):
-        if s[i] == s[i + 1]:
-            end_sum -= 1
-        end.append(end_sum)
-    end.append(0)
-    end.reverse()
-    m = int(input())
-    for i in range(m):
-        l, r = map(int, input().split())
-        print(start[l - 1] + end[r - 1] - sum)
+	end_sum = sum
+	end = [end_sum]
+	for i in range(len(s) - 2, 0, -1):
+		if s[i] == s[i + 1]:
+			end_sum -= 1
+		end.append(end_sum)
+	end.append(0)
+	end.reverse()
+	m = int(input())
+	for i in range(m):
+		l, r = map(int, input().split())
+		print(start[l - 1] + end[r - 1] - sum)
 
 ################################################################################
 # Fast IO
@@ -54,67 +54,67 @@ BUFSIZE = 8192
  
  
 class FastIO(IOBase):
-    newlines = 0
+	newlines = 0
  
-    def __init__(self, file):
-        self._fd = file.fileno()
-        self.buffer = BytesIO()
-        self.writable = "x" in file.mode or "r" not in file.mode
-        self.write = self.buffer.write if self.writable else None
+	def __init__(self, file):
+		self._fd = file.fileno()
+		self.buffer = BytesIO()
+		self.writable = "x" in file.mode or "r" not in file.mode
+		self.write = self.buffer.write if self.writable else None
  
-    def read(self):
-        while True:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            if not b:
-                break
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines = 0
-        return self.buffer.read()
+	def read(self):
+		while True:
+			b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+			if not b:
+				break
+			ptr = self.buffer.tell()
+			self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+		self.newlines = 0
+		return self.buffer.read()
  
-    def readline(self):
-        while self.newlines == 0:
-            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
-            self.newlines = b.count(b"\n") + (not b)
-            ptr = self.buffer.tell()
-            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
-        self.newlines -= 1
-        return self.buffer.readline()
+	def readline(self):
+		while self.newlines == 0:
+			b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+			self.newlines = b.count(b"\n") + (not b)
+			ptr = self.buffer.tell()
+			self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+		self.newlines -= 1
+		return self.buffer.readline()
  
-    def flush(self):
-        if self.writable:
-            os.write(self._fd, self.buffer.getvalue())
-            self.buffer.truncate(0), self.buffer.seek(0)
+	def flush(self):
+		if self.writable:
+			os.write(self._fd, self.buffer.getvalue())
+			self.buffer.truncate(0), self.buffer.seek(0)
  
  
 class IOWrapper(IOBase):
-    def __init__(self, file):
-        self.buffer = FastIO(file)
-        self.flush = self.buffer.flush
-        self.writable = self.buffer.writable
-        self.write = lambda s: self.buffer.write(s.encode("ascii"))
-        self.read = lambda: self.buffer.read().decode("ascii")
-        self.readline = lambda: self.buffer.readline().decode("ascii")
+	def __init__(self, file):
+		self.buffer = FastIO(file)
+		self.flush = self.buffer.flush
+		self.writable = self.buffer.writable
+		self.write = lambda s: self.buffer.write(s.encode("ascii"))
+		self.read = lambda: self.buffer.read().decode("ascii")
+		self.readline = lambda: self.buffer.readline().decode("ascii")
  
  
 def print(*args, **kwargs):
-    """Prints the values to a stream, or to sys.stdout by default."""
-    sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
-    at_start = True
-    for x in args:
-        if not at_start:
-            file.write(sep)
-        file.write(str(x))
-        at_start = False
-    file.write(kwargs.pop("end", "\n"))
-    if kwargs.pop("flush", False):
-        file.flush()
+	"""Prints the values to a stream, or to sys.stdout by default."""
+	sep, file = kwargs.pop("sep", " "), kwargs.pop("file", sys.stdout)
+	at_start = True
+	for x in args:
+		if not at_start:
+			file.write(sep)
+		file.write(str(x))
+		at_start = False
+	file.write(kwargs.pop("end", "\n"))
+	if kwargs.pop("flush", False):
+		file.flush()
  
  
 if sys.version_info[0] < 3:
-    sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
+	sys.stdin, sys.stdout = FastIO(sys.stdin), FastIO(sys.stdout)
 else:
-    sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+	sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
  
 input = lambda: sys.stdin.readline().rstrip("\r\n")
  
@@ -123,4 +123,4 @@ input = lambda: sys.stdin.readline().rstrip("\r\n")
 ################################################################################
 
 if __name__ == "__main__":
-    main()
+	main()
