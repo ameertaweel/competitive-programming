7d6d4555758cdae988e738fdf26af2d04275c5ce
Ameer Taweel <ameertaweel2002@gmail.com>
2022-04-26 19:17:41 +0300

    solve: leetcode/1584

---
+++ leetcode/1584.py
@@ -0,0 +1,42 @@
+import math
+
+class Solution:
+	# Use Optimized Prim's Algorithm
+	# Finds a minimum spanning tree (MST) of a graph
+	# Time Complexity: O(N^2)
+	# Space Complexity: O(N)
+	def minCostConnectPoints(self, points: List[List[int]]) -> int:
+		n = len(points)
+		
+		weights = [math.inf] * n
+		weights[0] = 0
+		
+		in_mst = [False] * n
+		
+		edges_used = 0
+		cost = 0
+		
+		while edges_used < n:
+			curr_edge = math.inf
+			curr_node = -1
+
+			for node in range(n):
+				if (not in_mst[node]) and weights[node] < curr_edge:
+					curr_edge = weights[node]
+					curr_node = node
+			
+			edges_used += 1
+			cost += curr_edge
+			in_mst[curr_node] = True
+			
+			for node in range(n):
+				if in_mst[node]:
+					continue
+				xi, yi = points[curr_node]
+				xj, yj = points[node]
+				new_weight = abs(xi - xj) + abs(yi - yj)
+				if new_weight < weights[node]:
+					weights[node] = new_weight
+			
+		
+		return cost
