fd6e898d550a505aa35146a8b7b3d8594d12a1fc
Ameer Taweel <ameertaweel2002@gmail.com>
2024-12-19 11:45:13 +0200

    advent of code 2024: solve day 19

---
+++ advent-of-code/2024/day-19/part-1.rs
@@ -0,0 +1,93 @@
+use std::collections::{HashMap, VecDeque};
+use std::io::{self, BufRead};
+
+fn main() {
+    let stdin = io::stdin();
+
+    let (patterns, designs) = {
+        let mut patterns = Trie::new();
+        let mut designs = Vec::new();
+        for (i, line) in stdin.lock().lines().enumerate() {
+            let line = line.unwrap();
+
+            if i == 0 {
+                for word in line.split(", ") {
+                    patterns.insert(word);
+                }
+                continue;
+            }
+
+            if i == 1 {
+                continue;
+            }
+
+            designs.push(line);
+        }
+        (patterns, designs)
+    };
+
+    let possible_designs_count: u64 = designs
+        .iter()
+        .map(|d| is_design_possible(&d, &patterns))
+        .map(|r| if r { 1 } else { 0 })
+        .sum();
+
+    println!("{}", possible_designs_count);
+}
+
+fn is_design_possible(design: &str, patterns: &Trie) -> bool {
+    let results = patterns.search(design);
+
+    if results.len() == 0 {
+        return false;
+    }
+
+    if results[0] == design.len() {
+        return true;
+    }
+
+    return results
+        .iter()
+        .map(|i| &design[*i..])
+        .any(|d| is_design_possible(d, patterns));
+}
+
+struct Trie {
+    children: HashMap<char, Trie>,
+    terminal: bool,
+}
+
+impl Trie {
+    fn new() -> Self {
+        Trie {
+            children: HashMap::new(),
+            terminal: false,
+        }
+    }
+
+    fn insert(&mut self, word: &str) {
+        let mut trie = self;
+        for c in word.chars() {
+            trie = trie.children.entry(c).or_insert_with(Trie::new);
+        }
+        trie.terminal = true;
+    }
+
+    fn search(&self, word: &str) -> VecDeque<usize> {
+        let mut trie = self;
+        let mut results = VecDeque::new();
+
+        for (i, c) in word.chars().enumerate() {
+            if let Some(sub_trie) = trie.children.get(&c) {
+                if sub_trie.terminal {
+                    results.push_front(i + 1);
+                }
+                trie = sub_trie;
+            } else {
+                return results;
+            }
+        }
+
+        return results;
+    }
+}
