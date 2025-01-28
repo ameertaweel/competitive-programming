d58d5facbbcf617b901a9d330283726c15bf0e42
Ameer Taweel <ameertaweel2002@gmail.com>
2025-01-28 15:48:04 +0200

    solve: leetcode/217

---
+++ leetcode/217.rs
@@ -0,0 +1,16 @@
+use std::collections::HashSet;
+
+impl Solution {
+    pub fn contains_duplicate(nums: Vec<i32>) -> bool {
+        let mut seen = HashSet::new();
+
+        for n in nums {
+            if seen.contains(&n) {
+                return true;
+            }
+            seen.insert(n);
+        }
+
+        return false;
+    }
+}
