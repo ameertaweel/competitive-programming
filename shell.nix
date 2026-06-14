8b6fae095aef6b88eeb76f82353727d8ae4f19f6
Ameer Taweel <ameertaweel2002@gmail.com>
2026-06-14 21:14:40 +0300

    drop nix flakes in favor of nixtamal

---
+++ shell.nix
@@ -0,0 +1,35 @@
+# Development Environment
+# You can activate it through:
+#   - New CLI: `nix develop --file shell.nix default`
+#   - Old CLI: `nix-shell -A default`
+{
+  pkgs ? (import ./nix/nixpkgs.nix),
+}:
+{
+  default = pkgs.mkShell {
+    nativeBuildInputs = let
+      tex-env = pkgs.texlive.combine {
+        inherit (pkgs.texlive) scheme-full latex-bin latexmk;
+      };
+    in [
+      # This project uses Nixtamal for input pinning
+      pkgs.nixtamal
+
+      # C / C++
+      pkgs.gcc
+
+      # Python
+      pkgs.python3
+
+      # Rust
+      pkgs.rustc
+
+      # Zig
+      pkgs.zig
+
+      # LaTeX Documents
+      # tex-env
+      # pkgs.python3Packages.pygments # For the minted package
+    ];
+  };
+}
